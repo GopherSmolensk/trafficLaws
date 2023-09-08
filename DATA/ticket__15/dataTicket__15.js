74c3b5e2bebd39823d0ef09f0e07378b8137e4ee
Dmitriy <dm.fred.nik@gmail.com>
2023-09-08 18:46:36 +0300

    refactoring

---
+++ DATA/ticket__15/dataTicket__15.js
@@ -0,0 +1,434 @@
+export const DATA = [
+  {
+    question: 'Вопрос №1: Какой неподвижный объект, не позволяющий продолжить движение по полосе, не относится к понятию «Препятствие»?',
+    link: './img/0-0.jpg',
+    answer: [
+      {
+        id: '1',
+        value: 'Дефект проезжей части.',
+        correct: false,
+      },
+      {
+        id: '2',
+        value: 'Посторонний предмет.',
+        correct: false,
+      },
+      {
+        id: '3',
+        value: 'Неисправное или поврежденное транспортное средство.',
+        correct: false,
+      },
+      {
+        id: '4',
+        value: 'Транспортное средство, остановившееся на этой полосе из-за образования затора.',
+        correct: true,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №2: Вам можно продолжить движение на перекрестке:',
+    link: './img/15-2.jpg',
+    answer: [
+      {
+        id: '5',
+        value: 'Только в направлении Б.',
+        correct: false,
+      },
+      {
+        id: '6',
+        value: 'В направлениях А и Б.',
+        correct: true,
+      },
+      {
+        id: '7',
+        value: 'В направлениях Б и В.',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №3: Этот дорожный знак:',
+    link: './img/15-3.jpg',
+    answer: [
+      {
+        id: '8',
+        value: 'Рекомендует двигаться со скоростью 40 км/ч.',
+        correct: false,
+      },
+      {
+        id: '9',
+        value: 'Требует двигаться со скоростью не менее 40 км/ч.',
+        correct: false,
+      },
+      {
+        id: '10',
+        value: 'Запрещает движение со скоростью более 40 км/ч.',
+        correct: true,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №4: Какие из указанных табличек указывают протяженность зоны действия знаков, с которыми они применяются?',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '11',
+        value: 'Только А.',
+        correct: false,
+      },
+      {
+        id: '12',
+        value: 'Только Б.',
+        correct: false,
+      },
+      {
+        id: '13',
+        value: 'Б и В.',
+        correct: true,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №5: Разрешен ли Вам такой маневр при выключенных реверсивных светофорах?',
+    link: './img/15-5.jpg',
+    answer: [
+      {
+        id: '14',
+        value: 'Разрешен.',
+        correct: false,
+      },
+      {
+        id: '15',
+        value: 'Разрешен, если нет встречных транспортных средств.',
+        correct: false,
+      },
+      {
+        id: '16',
+        value: 'Разрешен только для обгона.',
+        correct: false,
+      },
+      {
+        id: '17',
+        value: 'Запрещен.',
+        correct: true,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №6: При таких сигналах светофора и жесте регулировщика Вы должны:',
+    link: './img/15-6.jpg',
+    answer: [
+      {
+        id: '18',
+        value: 'Остановиться у стоп-линии.',
+        correct: true,
+      },
+      {
+        id: '19',
+        value: 'Продолжить движение только прямо.',
+        correct: false,
+      },
+      {
+        id: '20',
+        value: 'Продолжить движение прямо или направо.',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №7: Обязан ли в этой ситуации водитель, остановившийся из-за неисправности, выставить знак аварийной остановки?',
+    link: './img/15-7.jpg',
+    answer: [
+      {
+        id: '21',
+        value: 'Обязан.',
+        correct: true,
+      },
+      {
+        id: '22',
+        value: 'Обязан, если неисправна аварийная сигнализация.',
+        correct: false,
+      },
+      {
+        id: '23',
+        value: 'Не обязан.',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №8: По какой траектории Вам разрешено продолжить движение налево?',
+    link: './img/15-8.jpg',
+    answer: [
+      {
+        id: '24',
+        value: 'Только по А.',
+        correct: false,
+      },
+      {
+        id: '25',
+        value: 'Только по Б.',
+        correct: true,
+      },
+      {
+        id: '26',
+        value: 'По любой из указанных.',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №9: ',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №10: ',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №11: ',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №12: ',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №13: ',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №14: ',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №15: ',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №16: ',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №17: ',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №18: ',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №19: ',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №20: ',
+    link: './img/15-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+]
+
+export default DATA;
