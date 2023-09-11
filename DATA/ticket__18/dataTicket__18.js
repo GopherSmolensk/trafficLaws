6e31bd7e6c300d3bc174e881dbc077798cc98999
Dmitriy <dm.fred.nik@gmail.com>
2023-09-11 18:50:31 +0300

    added ticket 18

---
+++ DATA/ticket__18/dataTicket__18.js
@@ -0,0 +1,424 @@
+export const DATA = [
+  {
+    question: 'Вопрос №1: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №2: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №3: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №4: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №5: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №6: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №7: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №8: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №9: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №10: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №11: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №12: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №13: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №14: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №15: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №16: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №17: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №18: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №19: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+  {
+    question: 'Вопрос №20: ',
+    link: './img/18-1.jpg',
+    answer: [
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+      {
+        id: '',
+        value: '',
+        correct: true,
+      },
+      {
+        id: '',
+        value: '',
+        correct: false,
+      },
+    ]
+  },
+]
+
+export default DATA;
